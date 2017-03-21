7736d75a19fef929a6f5a201acee8816f61f216d
Atilla92 <atillajuliana@hotmail.com>
2017-03-21 16:28:02 +0100

    automate for one loop

--- Automate/computations.py
+++ Automate/computations.py
@@ -4,6 +4,10 @@ from random import randint
 from numpy import linalg as LA
 from scipy.spatial import ConvexHull
 import matplotlib.path as mltp
+import matplotlib.pylab as plt
+from intersection import sphere_line_intersection 
+from scipy.spatial import Delaunay
+from graphics import *
 
 def rand_obstacle(maxDis,maxRadius, numObj, SF1):
 	s =[]
@@ -78,3 +82,129 @@ def velocity_rotation(heading, velocity):
 	rot = rot.reshape(3,3)
 	vel_rot = np.dot(rot, velocity)
 	return vel_rot
+
+def unit_vectors(fov_x, fov_y, res_x, res_y):
+	phis   = np.linspace(-fov_x, fov_x, res_x)
+	thetas = np.linspace(-fov_y, fov_y, res_y)
+	layout = np.array([[phi, theta] 
+                   for theta in thetas
+                   for phi in phis])
+	# Compute unit vectors for each pixel
+	u = np.array([[ np.cos(theta) * np.cos(phi),
+                np.cos(theta) * np.sin(phi),
+                np.sin(theta)] for [phi, theta] in layout])
+	# Compute a: lateral vector of the base (a, b, u)
+	z = np.array([0, 0, 1])
+	a = np.cross(z[np.newaxis], u)
+	a /= np.linalg.norm(a, axis=1)[:,np.newaxis]
+	# Compute b: vertical vector of the base (a, b, u)
+	b = np.cross(u, a)
+	return u,a, b, layout 
+
+def display_vectors(X, V):
+    fig, axarr = plt.subplots(nrows=1, 
+                              ncols=2, 
+                              figsize=[10,5])
+    # Top view
+    for [ax, title, xlabel, ylabel, xx, yy, uu, vv] in zip(axarr, 
+                                                   ['Top view', 'Side view'],
+                                                   ['X', 'X'],
+                                                   ['Y', 'Z'],
+                                                   [X[0], X[0]],
+                                                   [X[1], X[2]],
+                                                   [V[0], V[0]],
+                                                   [V[1], V[2]]):
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
+        ax.quiver(xx, yy, uu, vv)
+
+    fig.tight_layout()
+
+
+def intersection_obstacles(unit, obstacle, position, ):
+
+	D = np.array([[sphere_line_intersection(position, u,  obs[0:3], obs[3]) for u in unit] for obs in obstacle]).T
+	Df = []
+	for d in D:
+	    if np.sum(d) > 0:
+	        Df.append(d[np.min(np.nonzero(d))])
+	    else:
+	        Df.append(0)
+	return Df
+
+def optic_flow(unit, D_vec,a,b,v):
+	mat = np.column_stack([unit, D_vec])
+	D_vec = np.array(D_vec)
+	p = []
+	for i in mat:
+	    if i[3]!= 0:       
+	        p.append( - (v - np.dot(i[0:3], v[np.newaxis,:].T) * i[0:3]) / (i[3])) 
+	        #print(p)
+	    if i[3]== 0:
+	        p.append([0,0,0])
+	p = np.array(p)
+	# # Project 3d optic flow vector on (a, b)
+	ofx = np.sum(p*a, axis=1)
+	ofy = np.sum(p*b, axis=1)
+	return ofx, ofy
+
+def delanauy_heading(position, obstacles,heading):
+	# Obstacles, radius, and position
+	points = np.array(obstacles)[:,0:2]
+	p = position[0:2]
+	r = np.array(obstacles) [:,3]
+	# Heading in cartesian coordinates
+	head = np.array([np.cos(heading), np.sin(heading)])
+	# Determine Delanauy triangles, simplices and respective points
+	tri = Delaunay(points)
+	median = points[tri.simplices]
+	# Determine in which triangle point is located
+	triangle_index = tri.find_simplex(p)
+	# Extract properties of this triangle, radius, points
+	points_index = points[tri.simplices[triangle_index]]
+	radius_index = r[tri.simplices[triangle_index]]
+	# Add last first element again for loop
+	rads= np.hstack([radius_index, radius_index[0]])
+	base = np.vstack([points_index, points_index[0]])
+	# Estimate median of triangle in which the drone is located
+	# Median with raidus taken into acount, and median without radius taken into account 
+	median =np.array([ [
+                     base[i], #0 first point [x,y]
+                     base[i+1], #1 next point [x,y]
+                    LA.norm(   #2 gap with radius scalar
+                        np.add(base[i],
+                            np.multiply(rads[i],
+                                    np.divide(([base[i+1]-base[i]]),
+                                             LA.norm([base[i+1]-base[i]]))))
+                            -
+                        np.add(base[i+1],
+                        np.multiply(-rads[i+1],
+                                    np.divide(([base[i+1]-base[i]]),
+                                             LA.norm([base[i+1]-base[i]]))))),
+                    LA.norm([base[i]- base[i+1]]), #3 gap without radius, scalar
+                    (np.add(base[i], # 4 midpoint with radius
+                            np.multiply(rads[i],
+                                    np.divide(([base[i+1]-base[i]]),
+                                             LA.norm([base[i+1]-base[i]]))))
+                                +
+                        np.add(base[i+1],
+                        np.multiply(-rads[i+1],
+                                    np.divide(([base[i+1]-base[i]]),
+                                             LA.norm([base[i+1]-base[i]])))))/2,
+                    (base[i]+base[i+1])/2 #5 midpoint without radius
+            
+                  
+                   ]
+
+                  for i in range(len(points_index)) ])
+	winner_index =median[np.argmax(median[:,2])]
+	dx_dy = winner_index[4]-p
+	y1 = dx_dy[0]
+	psi = np.arctan2(y1[1],y1[0])
+	if psi<0:
+	    psi = 2*np.pi+psi
+	new_heading = psi-heading 
+	return new_heading
--- Automate/main.py
+++ Automate/main.py
@@ -7,6 +7,7 @@ from numpy import linalg as LA
 from random import randint
 from graphics import *
 from computations import *
+from intersection import sphere_line_intersection
 
 # Initial restrictions on obstacles
 area = 10
@@ -24,14 +25,40 @@ maxVel = 20
 maxHeading = np.pi*2
 SF2 = 1 # safety factor
 
+# Set eye parameters:
+
+fov_x  = np.pi/2 #half FOV along azimuth in radians
+fov_y = np.pi/4 # half FOV along elevation in radians
+res_x = 100 #amount of pixels along azimuth
+res_y = 21 # amount of pixels along elevation
 
 
 # Do one sequence
 ## 1 Create Obstacles
+start_time = time.time()
 obstacles = rand_obstacle(maxDis,maxRadius, numObj, SF1)
 velocity = rand_velocity(maxVel)
 heading = rand_heading(maxHeading)
 position = rand_position(obstacles, area, SF2)
 obstacles_rot = object_rotation(heading, obstacles)
 velocity_rot = velocity_rotation(heading, velocity)
-print(obstacles, obstacles_rot, velocity, velocity_rot)
+u, a, b , layout= unit_vectors(fov_x, fov_y, res_x, res_y)
+intersection_points = intersection_obstacles(u, obstacles_rot, position )
+ofx, ofy = optic_flow(u, intersection_points, a,b, velocity_rot)
+heading_new = delanauy_heading(position, obstacles, heading)
+end_time = time.time()-start_time
+print('finished', end_time)
+# display_vectors(u.T, u.T)
+#display_vectors(u.T, b.T)
+
+#plt.show()
+print(obstacles, obstacles_rot.shape, velocity, velocity_rot,u.shape , a.shape, len(intersection_points), 
+	len(ofx), len(ofy),
+			heading_new)
+# plt.quiver(layout[:,0], layout[:,1], 
+#            ofx, ofy, scale =5)
+# plt.show()
+
+# plt.plot(np.array(intersection_points))
+# plt.show()
+# plt.close()
